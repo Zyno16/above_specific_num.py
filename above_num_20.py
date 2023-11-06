837c08245d40cdd44d4486080800448fe9fd9fe5
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-06 11:27:30 +0000

    Add files via upload

---
+++ above_num_20.py
@@ -0,0 +1,10 @@
+
+#this to sho thwe number above specific number in this example is display all number above 20
+count =0
+s=0
+for i in [1,7,37,45,54,67,74]:
+    if i> 20 :
+        print (i)
+
+
+
